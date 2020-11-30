c20c0ef9ba36e0a8f11d0765a870505a18341e31
Haron Acosta <haron.acosta14@gmail.com>
2020-11-30 11:15:16 -0400

    Seccion 1

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,19 @@
+// VS Code
+
+//Los comentario de pueden hacer en una linea 
+
+/* 
+========================
+O de varias lineas
+========================
+*/
+
+//Tomo el nombre del usuario y lo muestro en pantalal y consola
+const nombre = prompt("¿Cual es tu nombre?")
+
+console.log(nombre)
+
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo Javascript moderno`
+
+
+const producto =  `Monitor de 24 pulgadas`
