88457dc6fb8ced7e0416f312777a7c04ab123405
Haron Acosta <haron.acosta14@gmail.com>
2020-12-16 19:24:26 -0400

    Seccion 6 Booleanos

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,10 @@
+const autenticado = true;
+
+// if (autenticado) {
+//   console.log('Si puedes ver Netflix');
+// } else {
+//   console.log('No, no puedes ver Netflix');
+// }
+
+// Operador ternario
+console.log(autenticado ? 'Si esta autenticado' : 'No esta autenticado');
