6eca40049b689853e2e6cd9f43f61a6742416654
Haron Acosta <haron.acosta14@gmail.com>
2020-12-17 00:05:39 -0400

    Primera parte de objetos

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,15 @@
+const nombre = 'Monitor de 20 pulgadas';
+
+const precio = 300;
+
+const disponible = true;
+
+// Un objeto agrupa todo en una variable
+// Object Literal
+const producto = {
+  nombre: 'Monitor',
+  precio: 300,
+  disponible: true,
+};
+
+console.log(producto);
---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,9 @@
+// Acceder a los valores de un objeto
+
+const producto = {
+  nombre: 'Monitor',
+  precio: 300,
+  disponible: true,
+};
+
+console.log(producto.nombre);
